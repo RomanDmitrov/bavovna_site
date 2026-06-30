71a118d95332c836793f744a2b24f1d0f52b4ae7
Roman <162383532+RomanDmitrov@users.noreply.github.com>
2026-06-30 21:34:14 +0100

    Configurate Cloudinary for madia storege

--- config/settings.py
+++ config/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+from unittest.mock import DEFAULT
+
 from dotenv import load_dotenv
 import  dj_database_url
-
+import cloudinary
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / '.env')
@@ -36,12 +38,14 @@ CSRF_TRUSTED_ORIGINS = ['https://web-production-6f1e6d.up.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
+    'cloudinary_storage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
     'events',
     'bookings',
     'pages',
@@ -135,3 +139,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
