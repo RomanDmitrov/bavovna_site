f7274ef288cae152a1dbbd10f7379b48ae7f9d0b
Roman <162383532+RomanDmitrov@users.noreply.github.com>
2026-06-30 22:08:40 +0100

    Use whitenoise storage class for static files

--- config/settings.py
+++ config/settings.py
@@ -146,11 +146,5 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
 
-STORAGES = {
-    "default": {
-        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
